e228dbd2f36cb42b748dd521e22bd9dbc39a24a7
masums <masums@gmail.com>
2017-03-25 22:32:59 +0600

    Renamed the controller

---
+++ NetCoreCMS.Framework/Core/Mvc/Controllers/FrontController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreCMS.Framework.Core.Mvc.Controllers
+{
+    public class FrontController
+    {
+
+    }
+}
--- NetCoreCMS.Framework/Core/Mvc/Models/ModelBase.cs
+++ NetCoreCMS.Framework/Core/Mvc/Models/ModelBase.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NetCoreCMS.Framework.Core.Mvc
+namespace NetCoreCMS.Framework.Core.Mvc.Models
 {
-    public class NetCoreCmsController
+    class ModelBase
     {
-
     }
 }
