72fceaa5c1ee1d8677b6161322b6345e0fb84c64
masums <masums@gmail.com>
2017-04-14 15:49:57 +0600

    menu panel is designed.

--- NetCoreCMS.Web/Core/NetCoreCMS.Modules.Cms/wwwroot/js/menu.js
+++ NetCoreCMS.Web/Core/NetCoreCMS.Modules.Cms/wwwroot/js/menu.js
@@ -1,8 +1,17 @@
-﻿function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find(".more-less")
-        .toggleClass('glyphicon-plus glyphicon-minus');
-}
-$('.panel-group').on('hidden.bs.collapse', toggleIcon);
-$('.panel-group').on('shown.bs.collapse', toggleIcon);
+﻿$(document).ready(function () {
+
+    function toggleIcon(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+    }
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
+    
+    $("#selectAllPages").on("click", function () {
+        $(".tab-pane .active").find("input[type='checkbox']").each(function (i, ob) {
+            $(ob).prop("checked",true); 
+        });        
+    });
+});
