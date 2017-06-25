43584f5a06eaf3ebd4f9b1f7b5445ac5b44fd499
gmnazmul <gmnazmul@gmail.com>
2017-06-25 15:52:36 +0600

    Start working on media

---
+++ NetCoreCMS.Web/Core/NetCoreCMS.Modules.Cms/Controllers/CmsMediaController.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Author: TecRT
+ * Website: http://tecrt.com
+ * Copyright (c) tecrt.com
+ * License: BSD (3 Clause)
+*/
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetCoreCMS.Framework.Core.Models;
+using NetCoreCMS.Framework.Core.Network;
+using NetCoreCMS.Framework.Core.Services;
+using NetCoreCMS.Framework.Setup;
+using NetCoreCMS.Framework.Utility;
+using NetCoreCMS.Modules.Cms.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetCoreCMS.Core.Modules.Cms.Controllers
+{
+    [Authorize]
+    public class CmsMediaController : Controller
+    {
+        private readonly ILogger _logger;
+        public CmsMediaController(ILoggerFactory factory)
+        {
+            _logger = factory.CreateLogger<CmsMediaController>();
+        }
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Upload()
+        {            
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Upload(NccPage model, string PageContent, long ParentId, string SubmitType)
+        {
+            
+            return View();
+        }        
+
+        public ActionResult Delete(string fullPath)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string fullPath, int status)
+        {            
+            ViewBag.MessageType = "SuccessMessage";
+            ViewBag.Message = "Page deleted successful";
+            return RedirectToAction("Index");
+        }
+    }
+}
