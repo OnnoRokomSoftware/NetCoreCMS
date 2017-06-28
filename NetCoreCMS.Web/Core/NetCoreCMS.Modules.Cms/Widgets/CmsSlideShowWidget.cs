95f7c3879a2fc51daa2208841b404c0a2411e18a
masums <masums@gmail.com>
2017-06-28 12:02:36 +0600

    widget problem is solved.

--- NetCoreCMS.Web/Core/NetCoreCMS.Modules.Cms/Widgets/CmsSlideShowWidget.cs
+++ NetCoreCMS.Web/Core/NetCoreCMS.Modules.Cms/Widgets/CmsSlideShowWidget.cs
@@ -1,4 +1,6 @@
-﻿using NetCoreCMS.Framework.Modules.Widgets;
+﻿using NetCoreCMS.Framework.Core.Mvc.Views;
+using NetCoreCMS.Framework.Modules.Widgets;
+using NetCoreCMS.Modules.Cms.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,8 +16,9 @@ namespace NetCoreCMS.HelloWorld.Widgets
         string _configJson;
         string _description;
         string _viewFileName;
+        IViewRenderService _viewRenderService;
 
-        public string WidgetId { get { return "E082E185-2379-4C77-A026-FB73FFD09915"; } }
+        public string WidgetId { get { return "NetCoreCms.Modules.Cms.CmsSlideShow"; } }
 
         public string Title { get { return _title; } }
 
@@ -28,54 +31,22 @@ namespace NetCoreCMS.HelloWorld.Widgets
         public string ConfigJson { get { return _configJson; } }
         public string ViewFileName { get { return _viewFileName; } }
 
-        public CmsSlideShowWidget()
+        public CmsSlideShowWidget(IViewRenderService viewRenderService)
         {
-
+            _viewRenderService = viewRenderService;
         }
 
         public void Init()
         {
             _title = "Slide Show";
             _description = "Image Slide Show";
-            //_body = "<ul>" +
-            //            "<li><img src='/Media/Picture/?Id=32323' /></li>" + 
-            //            "<li><img src='/Media/Picture/?Id=32344' /></li>" +
-            //        "</ul>";
-            _body = "<div id=\"carousel-example-generic\" class=\"carousel slide\" data-ride=\"carousel\">" +
-                    "    <ol class=\"carousel-indicators\">" +
-                    "        <li data-target=\"#carousel-example-generic\" data-slide-to=\"0\" class=\"active\"></li>" +
-                    "        <li data-target=\"#carousel-example-generic\" data-slide-to=\"1\" class=\"\"></li>" +
-                    "        <li data-target=\"#carousel-example-generic\" data-slide-to=\"2\" class=\"\"></li>" +
-                    "        <li data-target=\"#carousel-example-generic\" data-slide-to=\"3\" class=\"\"></li>" +
-                    "    </ol>" +
-                    "    <div class=\"carousel-inner\" role=\"listbox\">" +
-                    "        <div class=\"item active\">" +
-                    "            <img src=\"/media/Images/2017/06/image-slider-0.jpg\" width=\"100%\" />" +
-                    "        </div>" +
-                    "        <div class=\"item\">" +
-                    "            <img src=\"/media/Images/2017/06/image-slider-1.jpg\" width=\"100%\" />" +
-                    "        </div>" +
-                    "        <div class=\"item\">" +
-                    "            <img src=\"/media/Images/2017/06/image-slider-2.jpg\" width=\"100%\" />" +
-                    "        </div>" +
-                    "        <div class=\"item\">" +
-                    "            <img src=\"/media/Images/2017/06/image-slider-3.jpg\" width=\"100%\" />" +
-                    "        </div>" +
-                    "    </div>" +
-                    "    <!--<a class=\"left carousel-control\" href=\"#carousel-example-generic\" role=\"button\" data-slide=\"prev\">" +
-                    "        <span class=\"glyphicon glyphicon-chevron-left\" aria-hidden=\"true\"></span>" +
-                    "        <span class=\"sr-only\">Previous</span>" +
-                    "    </a>" +
-                    "    <a class=\"right carousel-control\" href=\"#carousel-example-generic\" role=\"button\" data-slide=\"next\">" +
-                    "        <span class=\"glyphicon glyphicon-chevron-right\" aria-hidden=\"true\"></span>" +
-                    "        <span class=\"sr-only\">Next</span>" +
-                    "    </a>-->" +
-                    "</div>";
+            _viewFileName = "Widgets/CmsSlideShow";            
             _footer = "Footer";
         }
 
         public string RenderBody()
         {
+            _body = _viewRenderService.RenderToStringAsync<CmsWidgetController>(_viewFileName, null).Result;
             return _body;
         }
 
