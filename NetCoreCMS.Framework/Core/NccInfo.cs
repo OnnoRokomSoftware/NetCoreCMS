8f88c667406bc399aad92b0ce76cf9bf41b053ed
gmnazmul <gmnazmul@gmail.com>
2017-07-06 12:50:00 +0600

    bangladesh removed from name

--- NetCoreCMS.Framework/Core/NccInfo.cs
+++ NetCoreCMS.Framework/Core/NccInfo.cs
@@ -8,9 +8,9 @@ namespace NetCoreCMS.Framework.Core
         public static string Slogan { get; } = "An ASP.Net Core CMS as Site Engine";
         public static Version Version { get; } = new Version(0,2,2);
         public static string Description { get; } = "A Content Management System developed using ASP.NET Core.";
-        public static string Website { get; } = "http://NetCoreCMS-Bangladesh.com";
+        public static string Website { get; } = "http://NetCoreCMS.com";
         public static string Email { get; } = "netcorecms@gmail.com";
-        public static string Author { get; } = "NetCoreCMS-Bangladesh.com";
+        public static string Author { get; } = "NetCoreCMS.com";
         public static string CoreModuleFolder { get; } = "Core";
         public static string ModuleFolder { get; } = "Modules";
         public static string ThemeFolder { get; } = "Themes";
