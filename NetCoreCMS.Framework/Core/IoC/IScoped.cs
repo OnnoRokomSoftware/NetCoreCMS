b18d322a0fa428ca3a37338138394a4af785cf63
masums <masums@gmail.com>
2017-10-07 23:54:34 +0600

    logo added.

--- NetCoreCMS.Framework/Core/IoC/IScoped.cs
+++ NetCoreCMS.Framework/Core/IoC/IScoped.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace NetCoreCMS.Framework.Core.IoC
 {
+    /// <summary>
+    /// Implement this interface at Service and Repository for registering them into IoC container as Scoped.
+    /// </summary>
     public interface IScoped
     {
     }
--- NetCoreCMS.Framework/Core/IoC/ITransient.cs
+++ NetCoreCMS.Framework/Core/IoC/ITransient.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace NetCoreCMS.Framework.Core.IoC
 {
+    /// <summary>
+    /// Implement this interface at Service and Repository for registering them into IoC container as Transient.
+    /// If you do not implement any of three scope interface Ncc will register your repository or service as Transient by default.
+    /// </summary>
     public interface ITransient
     {
     }
