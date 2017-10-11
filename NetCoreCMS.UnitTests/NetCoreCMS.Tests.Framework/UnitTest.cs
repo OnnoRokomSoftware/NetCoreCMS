7bd593fa9a60b94696eb9d687c531e1a518301f9
masums <masums@gmail.com>
2017-10-11 15:34:12 +0600

    changed solution folder structure.

---
+++ NetCoreCMS.UnitTests/NetCoreCMS.Tests.Framework/UnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace NetCoreCMS.Tests.Framework
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal("Test", "Test");
+        }
+    }
+}
