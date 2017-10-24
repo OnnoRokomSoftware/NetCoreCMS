701a7e60fb91da16c7e6c13d2238c99f1ef3af60
masum <masums@gmail.com>
2017-10-24 18:33:50 +0600

    .Net Core xUnit Test added for NetCoreCMS.Framework project

---
+++ NetCoreCMS.UnitTests/NetCoreCMS.Framework.Test/HelloTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace NetCoreCMS.Framework.Test
+{
+    public class HelloTest
+    {
+        [Fact]
+        public void HelloTestWorld()
+        {
+            Assert.True(true);
+        }
+    }
+}
