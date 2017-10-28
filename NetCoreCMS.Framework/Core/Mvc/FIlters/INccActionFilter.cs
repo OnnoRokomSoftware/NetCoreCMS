9f984bda78c98f10e8ba24d4de37308231e62c52
masum <masums@gmail.com>
2017-10-28 19:47:55 +0600

    Added missing files.

---
+++ NetCoreCMS.Framework/Core/Mvc/FIlters/INccActionFilter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NetCoreCMS.Framework.Core.Mvc.FIlters
+{
+    public interface INccActionFilter : IActionFilter
+    {
+
+    }
+}
---
+++ NetCoreCMS.Framework/Core/Mvc/FIlters/NccExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace NetCoreCMS.Framework.Core.Mvc.FIlters
+{
+    public class NccGlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger _logger;
+
+        public NccGlobalExceptionFilter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<NccGlobalExceptionFilter>();
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception.ToString());
+        }
+    }
+}
